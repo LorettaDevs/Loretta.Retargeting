3ecd329a32245ee3f169588a0bfc1866ca6b6361
GGG KILLER <gggkiller2@gmail.com>
2022-04-03 07:06:26 -0300

    Add the tests project.

---
+++ Loretta.Retargeting.Test/Converters/ConverterTestsBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Loretta.CodeAnalysis.Lua;
+using Loretta.Retargeting.Core.Converters;
+
+namespace Loretta.Retargeting.Test.Converters
+{
+    public abstract class ConverterTestsBase
+    {
+        internal abstract ConverterBase CreateConverter();
+
+        protected void AssertConversion(string expected, LuaSyntaxOptions options, string input)
+        {
+            var converter = CreateConverter();
+            var inputNode = SyntaxFactory.ParseCompilationUnit(input, options: new LuaParseOptions(options));
+
+            var result = converter.Convert(inputNode, out var diags);
+
+            Assert.Empty(diags);
+            Assert.Equal(expected, result.ToFullString());
+        }
+    }
+}
