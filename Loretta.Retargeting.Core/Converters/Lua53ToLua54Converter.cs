a5b3e9b3d158fa669a010a19c4a4b9af32c3ebb4
GGG KILLER <gggkiller2@gmail.com>
2022-04-03 07:28:41 -0300

    Add the Lua 5.3 to Lua 5.4 converter.

---
+++ Loretta.Retargeting.Core/Converters/Lua53ToLua54Converter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Immutable;
+using Loretta.CodeAnalysis;
+using Loretta.CodeAnalysis.Lua;
+using Loretta.CodeAnalysis.Lua.Syntax;
+
+namespace Loretta.Retargeting.Core.Converters
+{
+    [Converter(LuaVersion.Lua53, LuaVersion.Lua54)]
+    internal sealed class Lua53ToLua54Converter : ConverterBase
+    {
+        public override SyntaxNode Convert(LuaSyntaxNode node, out ImmutableArray<Diagnostic> diagnostics)
+        {
+            var script = new Script(ImmutableArray.Create(node.SyntaxTree));
+            var rewriter = new Rewriter(script);
+            diagnostics = ImmutableArray<Diagnostic>.Empty;
+            return rewriter.Visit(node);
+        }
+
+        private sealed class Rewriter : LuaSyntaxRewriter
+        {
+            private readonly Script _script;
+
+            public Rewriter(Script script)
+            {
+                _script = script;
+            }
+
+            public override SyntaxNode? VisitLocalDeclarationName(LocalDeclarationNameSyntax node)
+            {
+                var variable = _script.GetVariable(node);
+                if (variable is not null && IsConst(variable, node.Parent!))
+                {
+                    var trailing = node.GetTrailingTrivia();
+                    node = node.WithoutTrailingTrivia();
+                    node = node.WithAttribute(SyntaxFactory.VariableAttribute("const"));
+                    node = node.WithTrailingTrivia(trailing);
+                    return node;
+                }
+                return base.VisitLocalDeclarationName(node);
+            }
+
+            private static bool IsConst(IVariable variable, SyntaxNode declaration)
+            {
+                if (!variable.WriteLocations.Any())
+                    return true;
+                if (variable.WriteLocations.Count() == 1 && variable.WriteLocations.Single() == declaration)
+                    return true;
+                return false;
+            }
+        }
+    }
+}
---
+++ Loretta.Retargeting.Test/Converters/Lua53ToLua54ConverterTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Loretta.CodeAnalysis.Lua;
+using Loretta.Retargeting.Core.Converters;
+
+namespace Loretta.Retargeting.Test.Converters
+{
+    public class Lua53ToLua54ConverterTests : ConverterTestsBase
+    {
+        internal override ConverterBase CreateConverter() => new Lua53ToLua54Converter();
+
+        [Fact]
+        public void Lua53ToLua54Converter_AnnotatesConstantVariables()
+        {
+            AssertConversion("""
+                local x<const> --[[comment]] = 1
+                print(x)
+                """,
+                LuaSyntaxOptions.Lua53,
+                """
+                local x --[[comment]] = 1
+                print(x)
+                """);
+        }
+    }
+}
