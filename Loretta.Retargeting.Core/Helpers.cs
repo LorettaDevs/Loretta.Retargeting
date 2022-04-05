b4e44b74bfd3d8683caae727a1174cfa049741b4
GGG KILLER <gggkiller2@gmail.com>
2022-04-05 20:21:14 -0300

    Fix multi line comments and add a few new methods.

--- Loretta.Retargeting.Core/Helpers.cs
+++ Loretta.Retargeting.Core/Helpers.cs
@@ -84,7 +84,7 @@ namespace Loretta.Retargeting.Core
             if (!s_commentCache.TryGetValue(text, out var objComment))
             {
                 var longestClosing = FindLongestClosing(text);
-                var equalsCount = Math.Max(longestClosing - 2, 0);
+                var equalsCount = Math.Max(longestClosing - 2 + 1, 0);
                 string commentText;
                 if (text.Length + longestClosing * 2 + 6 < maxStack)
                 {
@@ -125,7 +125,7 @@ namespace Loretta.Retargeting.Core
         public static void TurnIntoMultiLineComment(StringBuilder builder)
         {
             var longestClosing = FindLongestClosing(builder);
-            var equalsCount = Math.Max(longestClosing - 2, 0);
+            var equalsCount = Math.Max(longestClosing - 2 + 1, 0);
 
             builder.Insert(0, "--[")
                 .Insert(3, "=", equalsCount)
@@ -133,6 +133,50 @@ namespace Loretta.Retargeting.Core
                 .Append(']')
                 .Append('=', equalsCount)
                 .Append(']');
+        }
+
+        public static bool CanConvertToDouble(long value)
+        {
+            var converted = (long) (double) value;
+            return value == converted;
+        }
+
+        public static bool CanGeneratePrecisionLossAsDouble(long value)
+        {
+            return value - Math.BitDecrement(value) > 1
+                || Math.BitIncrement(value) - value > 1;
+        }
+
+        public static string EncodeToUtf8(long codepoint)
+        {
+            if (codepoint < 0x7F)
+            {
+                return $"\\x{codepoint:X2}";
+            }
+            else if (codepoint < 0x7FF)
+            {
+                // 00000000 00000yyy yyxxxxxx -> [ 110yyyyy 10xxxxxx ]
+                var byte01 = (byte) (0b11000000 | ((codepoint >> 06) & 0b11111));
+                var byte02 = (byte) (0b10000000 | (codepoint & 0b111111));
+                return $"\\x{byte01:X2}\\x{byte02:X2}";
+            }
+            else if (codepoint < 0xFFFF)
+            {
+                // 00000000 zzzzyyyy yyxxxxxx -> [ 1110zzzz 10yyyyyy 10xxxxxx ]
+                var byte01 = (byte) (0b11100000 | ((codepoint >> 12) & 0b001111));
+                var byte02 = (byte) (0b10000000 | ((codepoint >> 06) & 0b111111));
+                var byte03 = (byte) (0b10000000 | (codepoint & 0b111111));
+                return $"\\x{byte01:X2}\\x{byte02:X2}\\x{byte03:X2}";
+            }
+            else
+            {
+                // 000wwwzz zzzzyyyy yyxxxxxx -> [ 11110www 10zzzzzz 10yyyyyy 10xxxxxx ]
+                var byte01 = (byte) (0b11110000 | ((codepoint >> 18) & 0b000111));
+                var byte02 = (byte) (0b10000000 | ((codepoint >> 12) & 0b111111));
+                var byte03 = (byte) (0b10000000 | ((codepoint >> 06) & 0b111111));
+                var byte04 = (byte) (0b10000000 | (codepoint & 0b111111));
+                return $"\\x{byte01:X2}\\x{byte02:X2}\\x{byte03:X2}\\x{byte04:X2}";
+            }
         }
     }
 }
