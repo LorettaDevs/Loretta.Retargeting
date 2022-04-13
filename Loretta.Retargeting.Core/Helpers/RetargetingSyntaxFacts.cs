b6ca772dd07f840d4368ceed65fe41764cf6c587
GGG KILLER <gggkiller2@gmail.com>
2022-04-13 16:58:51 -0300

    Add a class for the retargeting syntax facts.

---
+++ Loretta.Retargeting.Core/Helpers/RetargetingSyntaxFacts.cs
@@ -0,0 +1,15 @@
+﻿using Loretta.CodeAnalysis.Lua;
+
+namespace Loretta.Retargeting.Core
+{
+    internal static class RetargetingSyntaxFacts
+    {
+        public static bool IsBitwiseExpression(SyntaxKind expressionKind) =>
+            expressionKind is SyntaxKind.BitwiseAndExpression
+                           or SyntaxKind.BitwiseOrExpression
+                           or SyntaxKind.BitwiseNotExpression
+                           or SyntaxKind.ExclusiveOrExpression
+                           or SyntaxKind.RightShiftExpression
+                           or SyntaxKind.LeftShiftExpression;
+    }
+}
