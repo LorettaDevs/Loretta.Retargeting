a3ba19eb5cd9b86483e55c2395571a0170353a4d
GGG KILLER <gggkiller2@gmail.com>
2023-01-14 21:35:50 -0300

    Move LuaSyntaxOptions mapping to LuaVersion.
    The System.Text.Json source generator doesn't know how to respect the JsonIgnore attribute so it was generating things with ReadOnlySpan<T> for some reason (at least I think that's the reason it was doing so).

--- src/Core/LuaVersion.cs
+++ src/Core/LuaVersion.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Loretta.CodeAnalysis.Lua;
+
 namespace Loretta.Retargeting.Core
 {
     /// <summary>
@@ -18,5 +20,23 @@ namespace Loretta.Retargeting.Core
         GLua = GMod,
         Luau,
         RobloxLua = Luau
+    }
+
+    public static class LuaVersionExtensions
+    {
+        public static LuaSyntaxOptions GetSyntaxOptions(this LuaVersion version) =>
+            version switch
+            {
+                LuaVersion.Lua51 => LuaSyntaxOptions.Lua51,
+                LuaVersion.Lua52 => LuaSyntaxOptions.Lua52,
+                LuaVersion.Lua53 => LuaSyntaxOptions.Lua53,
+                LuaVersion.Lua54 => LuaSyntaxOptions.Lua54,
+                LuaVersion.LuaJIT20 => LuaSyntaxOptions.LuaJIT20,
+                LuaVersion.LuaJIT21 => LuaSyntaxOptions.LuaJIT21,
+                LuaVersion.GMod => LuaSyntaxOptions.GMod,
+                LuaVersion.FiveM => LuaSyntaxOptions.FiveM,
+                LuaVersion.Luau => LuaSyntaxOptions.Luau,
+                _ => throw new ArgumentException($"Version {version} is not a valid lua version."),
+            };
     }
 }
--- src/Host/Model/ProjectFile.cs
+++ src/Host/Model/ProjectFile.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json.Serialization;
-using Loretta.CodeAnalysis.Lua;
 using Loretta.Retargeting.Core;
 
 namespace Loretta.Retargeting.Host.Model;
@@ -13,39 +12,14 @@ public sealed class ProjectFile
         string outputPath,
         IEnumerable<string> files)
     {
-        Files = files ?? throw new ArgumentNullException(nameof(files));
         InputVersion = inputVersion;
         OutputVersion = outputVersion;
+        Files = files ?? throw new ArgumentNullException(nameof(files));
         OutputPath = outputPath ?? throw new ArgumentNullException(nameof(outputPath));
-
-        InputParseOptions = GetParseOptions(inputVersion);
-        OutputParseOptions = GetParseOptions(outputVersion);
     }
 
     public LuaVersion InputVersion { get; }
     public LuaVersion OutputVersion { get; }
     public string OutputPath { get; }
     public IEnumerable<string> Files { get; }
-
-    [JsonIgnore]
-    public LuaParseOptions InputParseOptions { get; }
-    [JsonIgnore]
-    public LuaParseOptions OutputParseOptions { get; }
-
-    private static LuaParseOptions GetParseOptions(LuaVersion version)
-    {
-        return new LuaParseOptions(version switch
-        {
-            LuaVersion.Lua51 => LuaSyntaxOptions.Lua51,
-            LuaVersion.Lua52 => LuaSyntaxOptions.Lua52,
-            LuaVersion.Lua53 => LuaSyntaxOptions.Lua53,
-            LuaVersion.Lua54 => LuaSyntaxOptions.Lua54,
-            LuaVersion.LuaJIT20 => LuaSyntaxOptions.LuaJIT20,
-            LuaVersion.LuaJIT21 => LuaSyntaxOptions.LuaJIT21,
-            LuaVersion.GMod => LuaSyntaxOptions.GMod,
-            LuaVersion.FiveM => LuaSyntaxOptions.FiveM,
-            LuaVersion.Luau => LuaSyntaxOptions.Luau,
-            _ => throw new ArgumentException($"Version {version} is not a valid lua version."),
-        });
-    }
 }
